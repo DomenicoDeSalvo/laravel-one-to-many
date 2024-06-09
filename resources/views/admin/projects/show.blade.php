8e016851d125042be366307de08a92937ebab777
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-06-09 18:26:13 +0200

    Added show

---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('title')
+
+    {{$project->title}}
+
+@endsection
+
+@section('content')
+    <div class="container my-4">
+        <div class="d-flex justify-content-between my-3 ">
+                <a class='btn btn-secondary' href="{{route('admin.projects.edit', $project)}}">Modifica</a>
+            <form action="{{route ('admin.projects.destroy', $project)}}" method="POST">
+                @method('DELETE')
+                @csrf
+                <button class="btn-danger btn">Elimina</button>
+            </form>
+        </div>
+    </div>
+    <div class="container text-center">
+        <h2>{{$project->title}}</h2>    
+    </div>
+    <div class="container">
+        <div class="my-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title text-center">{{$project->starting_date}}</h5>
+                    <p class="card-text">{{$project->description}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
