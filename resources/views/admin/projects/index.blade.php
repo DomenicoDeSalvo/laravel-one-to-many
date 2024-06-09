6d970d6e24f59a63cd3432cb94b636b93ebf9f06
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-06-09 18:11:23 +0200

    Ìndex now functional

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -4,4 +4,44 @@
 
 @section('content')
 
+    <div class="container my-4">
+        <h1 class="text-center"> Il mio portfolio</h1>
+    </div>
+
+    <div class="container my-4">
+        <table class="table">
+            <thead>
+                <th>Numero</th>
+                <th>Titolo</th>
+                <th>Descrizione</th>
+                <th>Inizio progetto</th>
+                <th></th>
+            </thead>
+            <tbody>
+                @foreach ($projects as $project)
+                <tr>
+                    <td>
+                        {{$project->id}}
+
+                    </td>
+                    <td>
+                        <a href="{{route('admin.projects.show', $project)}}">{{$project->title}}</a>
+                    </td>
+                    <td>
+                        {{$project->description}}
+                    </td>
+                    <td>
+                        {{$project->starting_date}}
+                    </td>
+                    <td>
+                        <button class="btn">1</button>
+                        <button class="btn">2</button>
+
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
 @endsection
