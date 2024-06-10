d85fbd980b2882bf4dbb0328c965d03e65f16584
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-06-10 12:20:25 +0200

    Added foreign key to ProjectSeeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,16 +16,21 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-        //
+        
+        $types = Type::all();
+        $ids = $types->pluck('id')->all();
+
         for($i = 0; $i < 20; $i++){
             
             $project = new Project();
-            $title = $faker->sentence(10);
+
+            $title = $faker->sentence(5);
             $project -> title = $title;
             $project -> slug = Str::slug($title);
             $project -> description = $faker->text(200);
             $project -> starting_date = $faker->dateTime();
             $project -> link = $faker->url();
+            $project -> type_id = $faker->optional()->randomElement($ids);
 
             $project->save();
         }
